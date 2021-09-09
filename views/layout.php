3423bf6783bdde82ed80437bfd5bf7f7832f2078
fabricio bencomo <fabriciobencomo@MacBook-Air-de-fabricio.local>
2021-09-09 19:44:49 -0400

    qpaso

--- views/layout.php
+++ views/layout.php
@@ -3,12 +3,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/build/">
+    <link rel="stylesheet" href="../public/build/css/app.css">
     <title>Devniors</title>
 </head>
 <body>
-    <main>
-        
-    </main>
+    <header class="header inicio">
+        <div class="nav">
+            
+        </div>
+    </header>
 </body>
 </html>
